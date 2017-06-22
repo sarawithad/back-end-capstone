9a05d403ddb86660da9175b75df58ca76bdfd577
Dara Thomas <darathomas@Daras-MacBook-Air.local>
2017-06-22 02:08:31 -0500

    beginnings of scraping

---
+++ scraping/scrape1.py
@@ -0,0 +1,24 @@
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import pandas.io.sql as pd_sql
+from pprint import pprint
+import sqlite3 as sql
+
+def scrape_site(url):
+
+    response = requests.get(url)
+    html = BeautifulSoup(response.content, 'html.parser')
+
+
+def write_to_sqlite(list_obj):
+
+    con = sql.connect("../db.sqlite3")
+
+
+
+bonnaroo_url = "https://www.bonnaroo.com/lineup"
+
+bonnaroo_data = scrape_site(bonnaroo_url)
+write_to_sqlite(bonnaroo_data)
